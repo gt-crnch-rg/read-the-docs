c1cf8a3d3d8d960cc5d1f8c722f97b3962c84b49
Jeffrey Young <jyoung3131@users.noreply.github.com>
2023-08-06 21:13:08 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CRNCH Rogues Gallery'
-copyright = u'2020, CRNCH'
+copyright = u'2023, CRNCH'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
